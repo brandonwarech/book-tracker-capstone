16f2a9af26a5558e5b65858d930c88a4c58689dc
brandonwarech <34520013+brandonwarech@users.noreply.github.com>
2019-06-05 20:38:07 -0400

    test

--- book_search.py
+++ book_search.py
@@ -1,7 +1,7 @@
 import sys
 import requests
 
-headers = {"accept": "application/json" }
+headers = {"accept": "application/json"}
 search_url = "http://openlibrary.org/search.json?"
 books_url = "https://openlibrary.org/api/books?jscmd=data&format=JSON"
 isbn_array = []
@@ -11,7 +11,7 @@ final_books_array = []
 def search(params):
     print("Starting")
     # url = "https://openlibrary.org/api/books?jscmd=data&format=JSON&bibkeys=ISBN:9780980200447"
-        
+
     if "isbn" in params:
         isbn = params["isbn"]
         data = search_by_isbn(isbn)
@@ -19,7 +19,7 @@ def search(params):
         return {
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
-            "body": {"books": [book_object]}
+            "body": {"books": [book_object]},
         }
 
     elif "author" in params:
@@ -47,21 +47,22 @@ def search(params):
             "headers": {"Content-Type": "application/json"},
             "body": {"books": [final_books_array]},
         }
-    
+
     else:
-        print('error: No parameters supplied')
+        print("error: No parameters supplied")
         return {
             "statusCode": 404,
             "headers": {"Content-Type": "application/json"},
-            "body": {"error": 'Please provide search parameter'}
+            "body": {"error": "Please provide search parameter"},
         }
+
 
 def transform_books_to_output_format(data):
     for d in data:
         try:
             title = data[d]["title"]
             publisher = data[d]["publishers"][0]["name"]
-            isbn = data[d]["identifiers"]['isbn_10'][0]
+            isbn = data[d]["identifiers"]["isbn_10"][0]
             url = data[d]["url"]
             author_array = []
             authors = data[d]["authors"]
@@ -74,12 +75,13 @@ def transform_books_to_output_format(data):
                 "author": author_array,
                 "publisher": publisher,
                 "publication_date": publication_date,
-                "url": url
+                "url": url,
             }
             print(book_object)
             return book_object
         except KeyError:
             pass
+
 
 def search_by_isbn(isbn):
     books_url_final = books_url + "&bibkeys=ISBN:" + isbn
@@ -89,50 +91,52 @@ def search_by_isbn(isbn):
         data = r.json()
         return data
     except requests.exceptions.HTTPError as errh:
-        print ("Http Error:",errh)
+        print("Http Error:", errh)
     except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:",errc)
+        print("Error Connecting:", errc)
     except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:",errt)
+        print("Timeout Error:", errt)
     except requests.exceptions.RequestException as err:
-        print ("OOps: Something Else",err)
+        print("OOps: Something Else", err)
+
 
 def open_query_to_isbn_array(query):
-    search_url_final = search_url + 'q=' + query
-    r = requests.get(search_url_final,headers)
+    search_url_final = search_url + "q=" + query
+    r = requests.get(search_url_final, headers)
     data = r.json()
     print(data)
 
+
 def author_query_to_isbn_array(author):
-    search_url_final = search_url + 'author=' + author
+    search_url_final = search_url + "author=" + author
     print(search_url_final)
-    r = requests.get(search_url_final,headers)
+    r = requests.get(search_url_final, headers)
     data = r.json()
-    for d in data['docs']:
+    for d in data["docs"]:
         try:
-            isbn_array.append(d['isbn'][0])
+            isbn_array.append(d["isbn"][0])
         except KeyError:
             pass
     print(isbn_array)
     return isbn_array
+
 
 def title_query_to_isbn_array(title):
-    search_url_final = search_url + 'title=' + title
+    search_url_final = search_url + "title=" + title
     print(search_url_final)
-    r = requests.get(search_url_final,headers)
+    r = requests.get(search_url_final, headers)
     data = r.json()
-    for d in data['docs']:
+    for d in data["docs"]:
         try:
-            isbn_array.append(d['isbn'][0])
+            isbn_array.append(d["isbn"][0])
         except KeyError:
             pass
     print(isbn_array)
     return isbn_array
 
-#main({'author':'Rowling'})
-#main({"isbn": "9780980200447"})
-#main({"title": "a child called it"})
-#search({'test':'test'})
 
-
+# search({'author':'Rowling'})
+search({"isbn": "9780980200447"})
+# search({"title": "a child called it"})
+# search({'test':'test'})
 
